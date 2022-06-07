175cab2e04ef3a56376e8739e1ec74f0c25dbfc6
‘haoyue’ <1762351793@qq.com>
2022-06-07 16:49:03 +0800

    2

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,14 @@
 module.exports = {
     themeConfig: {
-        base: '/MUI',
+        title: 'M-UI',
+        base: '/MUI/',
+        themConfig: {
+            nav: [
+                { text: 'Home', link: '/' },
+                { text: 'Github', link: 'https://github.com/haoyue886/MUI' },
+                { text: 'Npm', link: 'https://www.npmjs.com/package/m-ui0' }
+            ]
+        },
         sidebar: [
             '/',
             '/componentDocs/button',
